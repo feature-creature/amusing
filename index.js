9478ad9f2dd5bebbf4930cb8fb497619c283aa6e
Feature Creature <jdema003@gold.ac.uk>
2018-10-06 22:47:03 +0100

    adds udp server and osc emitted over socket

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var osc = require("osc-min");
+var dgram = require("dgram");
+
 
 // create route handler for home
 app.get("/",function(req,res){
@@ -11,13 +14,34 @@ app.get("/",function(req,res){
 // listen for incoming sockets on the http server
 io.on("connection", function(socket){
     console.log("a user connected");
+
     // client-side - socket disconnect
     socket.on("disconnect",function(){
         console.log("user disconnected");
     });
 });
 
-// port
-http.listen(3000, function(){
-   console.log("listening on :3000");
+
+// intialize upd server for osc messages
+var udp_server = dgram.createSocket("udp4", function(msg,rinfo){
+    var osc_msg;
+    try{
+        osc_msg = osc.fromBuffer(msg);
+    } catch(err){
+        return console.log("could not decode OSC message");
+    }
+
+    // emit osc message to websocket clients
+    io.emit("osc",osc_msg);
+
 });
+
+
+// have the http server listen on port 3000
+http.listen(3000, function(){
+   console.log("http listening on :3000");
+});
+
+// have the udp server listen on port 4500
+udp_server.bind(4500);
+console.log("udp listening on :4500");
