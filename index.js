63b498c27a2138b851151b5e8b6675bc3ac96d1d
Feature Creature <jdema003@gold.ac.uk>
2018-10-06 22:03:02 +0100

    adds socketio listener

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-// initialize http server
+// initialize http server and socket.io instance
 var app = require("express")();
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 // create route handler for home
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
+});
+
+// listen for incoming sockets on the http server
+io.on("connection", function(socket){
+    console.log("a user connected");
+    // client-side - socket disconnect
+    socket.on("disconnect",function(){
+        console.log("user disconnected");
+    });
 });
 
 // port
