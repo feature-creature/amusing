069b29579d6b38da65637687892bbb721c16497b
Feature Creature <jdema003@gold.ac.uk>
2018-10-07 10:56:14 +0100

    faulty iterator

--- index.js
+++ index.js
@@ -22,65 +22,37 @@ io.on("connection", function(socket){
 });
 
 
-// intialize headset 1 upd server for osc messages
-var udp_server_headset_1 = dgram.createSocket("udp4", function(msg,rinfo){
-    var osc_msg;
-    try{
-        osc_msg = osc.fromBuffer(msg);
-    } catch(err){
-        return console.log("could not decode OSC message");
-    }
+// intialize a udp server for each headset's osc messages
+var numOfHeadsets = 16;
+var headsetUDPServers = [];
 
-    // emit osc message to websocket clients
-    io.emit("osc",{
-       "message": osc_msg,
-       "headset":"1"
+for(var i = 0; i < numOfHeadsets; i++){
+    headsetUDPServers[i] = dgram.createSocket("udp4", function(msg,rinfo){
+        var osc_msg;
+        var headsetNum = i;
+        try{
+            osc_msg = osc.fromBuffer(msg);
+        } catch(err){
+            return console.log("could not decode OSC message");
+        }
+
+        // emit osc message to websocket clients
+        io.emit("osc",{
+            "message": osc_msg,
+            "headset": headsetNum
+        });
     });
-});
 
-// intialize headset 2 upd server for osc messages
-var udp_server_headset_2 = dgram.createSocket("udp4", function(msg,rinfo){
-    var osc_msg;
-    try{
-        osc_msg = osc.fromBuffer(msg);
-    } catch(err){
-        return console.log("could not decode OSC message");
-    }
-
-    // emit osc message to websocket clients
-    io.emit("osc",{
-       "message": osc_msg,
-       "headset":"2"
-    });
-});
-
-// intialize headset 3 upd server for osc messages
-var udp_server_headset_3 = dgram.createSocket("udp4", function(msg,rinfo){
-    var osc_msg;
-    try{
-        osc_msg = osc.fromBuffer(msg);
-    } catch(err){
-        return console.log("could not decode OSC message");
-    }
-
-    // emit osc message to websocket clients
-    io.emit("osc",{
-       "message": osc_msg,
-       "headset":"3"
-    });
-});
-
+}
 
 // have the http server listen on port 3000
 http.listen(3000, function(){
-   console.log("http listening on :3000");
+   console.log("http listening on port 3000");
 });
 
 // have the udp servers for each room listen on their dedicated ports
-udp_server_headset_1.bind(4501);
-udp_server_headset_2.bind(4502);
-udp_server_headset_3.bind(4503);
-
-console.log("room 1 listening on :4501");
-console.log("room 2 listening on :4502");
-console.log("room 2 listening on :4503");
+for(var i = 0; i < numOfHeadsets; i++){
+    var portNum = 4500+i;
+    headsetUDPServers[i].bind(portNum);
+    console.log("headset " + i + " listening on port "+ portNum);
+}
